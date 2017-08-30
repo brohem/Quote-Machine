fae29c24c03f2647ef83e083846c77ce15c57880
brohem <brohem@gmail.com>
2017-08-30 20:32:17 +0300

    First Commit

---
+++ resources/js/main.js
@@ -0,0 +1,42 @@
+function main(){
+
+$('#newQuote').click( function(){
+
+
+  $.ajax({
+      url: "http://api.forismatic.com/api/1.0/",
+      jsonp: "jsonp",
+      dataType: "jsonp",
+      data: {
+        method: "getQuote",
+        lang: "en",
+        format: "jsonp"
+      },
+      success: function(data) {
+        console.log(data);
+          $(".qoute-row").hide().html('<p class=qoute>' + data.quoteText + '</p>').fadeIn(1000);
+          $(".author-row").hide().html('<p class=author> - ' + data.quoteAuthor + '</p>').fadeIn(1500);
+      },
+
+      function(err) {
+        console.log('Error: ' + err.status);
+      }
+
+    });
+
+  });// END #newQuote click function
+
+$('#tweet-quote').click(function(){
+
+ $(this).attr("href", 'https://twitter.com/intent/tweet?text=' + data.quoteText + " -by " + data.quoteAuthor);
+
+
+});
+
+
+//<script async src="//platform.twitter.com/widgets.js" charset="utf-8"></script>
+
+};//END main function
+
+
+$(document).ready(main);
